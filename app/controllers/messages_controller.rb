80d9409fe8b10b247a1d9e72b1ae47c782175cd0
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-10-31 02:41:03 +0000

    4th commit

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,14 @@
+class MessagesController < ApplicationController
+  def create
+    @message = Message.new(message_params)
+    @message.user_id = current_user.id
+    unless @message.save
+      flash[:notice] = "140文字以内で送信してください。"
+    end
+    redirect_to request.referer
+  end
+  
+  def message_params
+    params.require(:message).permit(:room_id, :content)
+  end
+end
