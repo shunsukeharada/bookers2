13bd92d4625c5d09664e4c5caabafde6ab0004d1
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-11-02 07:10:28 +0000

    11th commit

--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2022_10_31_050325) do
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.integer "star"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
